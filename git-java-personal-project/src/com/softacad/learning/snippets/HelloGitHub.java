4388c7d2fe8c343fcd1f6157cb4e9ece76721880
PSG-Gaming <dimitar_apostolov_1992@abv.bg>
2016-04-12 19:22:02 +0300

    Initial commit

---
+++ git-java-personal-project/src/com/softacad/learning/snippets/HelloGitHub.java
@@ -0,0 +1,9 @@
+package com.softacad.learning.snippets;
+
+public class HelloGitHub {
+
+	public static void main(String[] args) {
+		System.out.println("best teacher is Boby. My first git steps");
+	}
+	
+}
